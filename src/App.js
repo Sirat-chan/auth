19ce1d69b98cb43fab3c8d87c5ebe5f7648b5e00
Sirat-chan <sirat.benjemaa@edu.isetcom.tn>
2023-03-21 23:45:48 +0100

    Authentification first steps frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Redirect,
+    Switch
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import Users from './user/pages/Users';
+import Auth from './user/pages/Auth';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+import { AuthContext } from './shared/context/auth-context';
+import { useAuth } from './shared/hooks/auth-hook';
+
+const App = () => {
+    const { token, login, logout, userId } = useAuth();
+
+    let routes;
+
+    if (token) {
+        routes = (
+            <Switch>
+                <Route path="/" exact>
+                    <Users />
+                </Route>
+
+                <Redirect to="/" />
+            </Switch>
+        );
+    } else {
+        routes = (
+            <Switch>
+                <Route path="/" exact>
+                    <Users />
+                </Route>
+
+                <Route path="/auth">
+                    <Auth />
+                </Route>
+                <Redirect to="/auth" />
+            </Switch>
+        );
+    }  //Overall, this component renders a different set of routes based on the authentication status.
+    // If the user is authenticated, it renders protected routes, otherwise it renders public routes with an authentication route.
+
+    return (
+        <AuthContext.Provider
+            value={{
+                isLoggedIn: !!token,
+                token: token,
+                userId: userId,
+                login: login,
+                logout: logout
+            }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+            <Router>
+                <MainNavigation />
+                <main>{routes}</main>
+            </Router>
+        </AuthContext.Provider>
+    );
+    // sets up the authentication context for the app using the AuthContext.
+    // Provider, sets up the routing using the Router component, and renders the main navigation and the main content of the app based on the current URL path using the MainNavigation and routes components.
+};
 
 export default App;
